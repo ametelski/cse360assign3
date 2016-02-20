9da80b29f4cd7c600222e113edddb43bd470eb4b
Adam Metelski <asmetelski@gmail.com>
2016-02-20 09:14:55 -0700

    First submit

---
+++ src/cse360assign3/Calculator.java
@@ -0,0 +1,72 @@
+
+package cse360assign3;
+
+/**
+ * This is a simple class for calculating a value by adding, subtracting, multiplying, and dividing. This program will also
+ * track the history of commands. 
+ * 
+ * @author Adam Metelski pin number 605 
+ * @version 2016.02.20
+ *
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Constructor for Calculator class. Sets total to zero. 
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This will return the current total stored. 
+	 * 
+	 * @return current total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Method will add to the total value
+	 * @param value of type int
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Method will subtract value from current total
+	 * @param value of type int 
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Method will multiply value with current total 
+	 * @param value  of type int
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Method will divide current total by value
+	 * @param value of type int
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Method will return the history of commands executed 
+	 * @return empty string
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,58 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Junit test for Calculator class
+ * 
+ * @author Adam Metelsk pin number 605 
+ * @version 2016.02.20
+ *
+ */
+public class CalculatorTest {
+
+	@Test
+	public void test_Calculator() {
+		Calculator test = new Calculator(); 
+		
+		assertNotNull(test); 
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	
+}
